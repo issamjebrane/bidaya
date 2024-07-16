954083ceca04aa00febd8f63ff329f67541bddbf
issamjebrane <issamjebrane65@gmail.com>
2024-07-16 16:13:56 +0100

    minor changes

--- src/main/java/com/bidaya/bidaya/config/CorsConfig.java
+++ src/main/java/com/bidaya/bidaya/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://bidaya.vercel.app")
+                .allowedOrigins("https://bidaya.vercel.app", "http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
